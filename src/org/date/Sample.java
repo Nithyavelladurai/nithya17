7e3674a270fcfb0bbc66c35b6e48b5d2ee6b6ca5
Nithyavelladurai <nithyavelladurai17@gmail.com>
2022-04-26 22:38:52 +0530

    nithyaa

---
+++ src/org/date/Sample.java
@@ -0,0 +1,49 @@
+package org.date;
+
+public class Sample {
+public static void main(String[] args) {
+	byte age=22;
+	System.out.println(age);
+	short year=2791;
+	System.out.println(year);
+	int phoneno=987654332;
+	System.out.println(phoneno);
+	long aadharno=99471810089l;
+	System.out.println(aadharno);
+	float x=45/6;
+	System.out.println(x);
+	double y=798/9;
+	System.out.println(y);
+	char gender='f';
+	System.out.println(gender);
+	boolean n=true;
+	System.out.println(n);
+	String email="nithyavelladurai17@gmail.com";
+	System.out.println(email);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+}
